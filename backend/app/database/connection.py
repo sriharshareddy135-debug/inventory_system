86649c36884aeea45d20810c75776913c2fb08b1
sri harsha <sriharshareddy135@gmail.com>
2026-07-18 22:02:41 +0530

    Update connection.py

--- backend/app/database/connection.py
+++ backend/app/database/connection.py
@@ -8,7 +8,11 @@ DATABASE_URL = os.getenv(
     "postgresql://postgres:password@localhost:5432/inventory_db"
 )
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
